bce8709d4b7caa0a308a977a1ae731baae02921b
Andrea Jüttner <75210531+andreajuettner@users.noreply.github.com>
2025-11-26 09:12:11 +0100

    Add German language strings for local_cohortrole

---
+++ lang/de/local_cohortrole.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  2025 Andrea Jüttner
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Plugin language strings (German)
+ *
+ * @package    local_cohortrole
+ * @copyright  2013 Paul Holden <paulh@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['categorycontext'] = 'Kategorie: {$a}';
+$string['cohort'] = 'Globale Gruppe';
+$string['cohort_help'] = 'Liste der globalen Gruppen auf Systemebene oder in Kursbereichen. Die Rolle wird immer im Systemkontext zugewiesen.';
+$string['cohortcontext'] = 'Kontext der Gruppe';
+$string['deleteconfirm'] = 'Möchten Sie diese Synchronisation wirklich löschen?';
+$string['errorexists'] = 'Synchronisation bereits definiert';
+$string['eventdefinitioncreated'] = 'Globale Gruppen-Rollensynchronisation erstellt';
+$string['eventdefinitiondeleted'] = 'Globale Gruppen-Rollensynchronisation gelöscht';
+$string['heading_add'] = 'Neue Synchronisation definieren';
+$string['heading_delete'] = 'Definierte Synchronisation löschen';
+$string['heading_index'] = 'Aktuell definierte Synchronisationen';
+$string['notificationcreated'] = 'Neue Synchronisation erstellt';
+$string['notificationdeleted'] = 'Synchronisation gelöscht';
+$string['pluginname'] = 'Globale Gruppen-Rollensynchronisation';
+$string['privacy:metadata:cohortrole'] = 'Enthält Synchronisationsdefinitionen von globalen Gruppen zu Rollen';
+$string['privacy:metadata:cohortrole:cohortid'] = 'Die ID der globalen Gruppe';
+$string['privacy:metadata:cohortrole:roleid'] = 'Die ID der Rolle';
+$string['privacy:metadata:cohortrole:timecreated'] = 'Der Zeitstempel der Erstellung der Definition';
+$string['privacy:metadata:cohortrole:usermodified'] = 'Die ID des Nutzers, der die Definition erstellt hat';
+$string['role'] = 'Rolle';
+$string['role_help'] = 'Liste der zuweisbaren Rollen. Die Rolle wird immer im Systemkontext zugewiesen, unabhängig davon, wo sich die globale Gruppe befindet.';
+$string['systemcontext'] = 'System';
